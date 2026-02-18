779b14e5069330aaaad4bd1a5040a68084280978
Валера Гоголев <v1rus1500@yandex.ru>
2026-02-18 09:23:14 +0300

    Добавьте файлы проекта.

---
+++ MarmaladeApp/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MarmaladeApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MarmaladeApp
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Gogolev_marmaladeEntities context = new Gogolev_marmaladeEntities();
+    }
+}
---
+++ MarmaladeApp/View/Windows/AuthorizationWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using MarmaladeApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MarmaladeApp.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void RegBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationWindow registrationWindow = new RegistrationWindow();
+            registrationWindow.Show();
+            this.Close();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginTB.Text) || string.IsNullOrEmpty(PassPB.ToString()))
+            {
+                MessageBox.Show("Введите логин и/или пароль", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+
+                if (App.context.User.FirstOrDefault(user => user.Login == LoginTB.Text && user.Password == PassPB.Password)!=null)
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+
+                    MessageBox.Show("Неверные логин и/или пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
